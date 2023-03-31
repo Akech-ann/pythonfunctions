c40d3ce8148d70fd0d7046bfb6eaf5e1bd2dc6b4
Akech-ann <akechannatong@gmail.com>
2023-03-31 15:30:06 +0300

    python functions

---
+++ math.py
@@ -0,0 +1,19 @@
+def add(num1,num2):
+    answer = num1+num2
+    return answer
+
+def subtract(num1,num2):
+    answer=num1-num2
+    return answer 
+
+def divide(num1,num2):
+    answer = num1/num2
+    return answer
+
+def multiply(num1,num2):
+    answer = num1*num2
+    return answer
+
+def remainder(num1,num2):
+    answer = num1%num2
+    return answer               
